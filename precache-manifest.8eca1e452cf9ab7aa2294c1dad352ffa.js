11ddc3d23f001c8619ccd90f19a8320c66de201f
Kent Mossbäck <kent.mossback@scb.se>
2024-01-04 17:48:27 +0100

    Updates

---
+++ precache-manifest.8eca1e452cf9ab7aa2294c1dad352ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c5504f98e072cbf17562df70728e03",
+    "url": "/simplereacttodeploy/index.html"
+  },
+  {
+    "revision": "9382772e89f0a248eb7b",
+    "url": "/simplereacttodeploy/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0be7aad1ca9b8854eee4",
+    "url": "/simplereacttodeploy/static/js/2.43f2ee7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simplereacttodeploy/static/js/2.43f2ee7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9382772e89f0a248eb7b",
+    "url": "/simplereacttodeploy/static/js/main.2c72b389.chunk.js"
+  },
+  {
+    "revision": "638d66fa371c63cffbc3",
+    "url": "/simplereacttodeploy/static/js/runtime-main.f8f9b85e.js"
+  }
+]);
